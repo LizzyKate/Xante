f3014f1e84ef70b6a66685c6842827f69c5b4b01
LizzyKate <onyenekwelizabeth@gmail.com>
2021-09-18 23:15:02 +0100

    Working on home page

---
+++ components/home.vue
@@ -0,0 +1,146 @@
+<template>
+  <section class="pt-12 cover">
+    <div class="flex wrapper items-center justify-between" style="">
+      <div class="w-1/2 crypto">
+        <!-- mb-20 -->
+        <h1 class="font-bold">
+          Smart & Intelligent Search across Cryptocurrency & Traditional Markets
+          for the best return on your assets.
+        </h1>
+        <p class="mt-6 font-thin">
+          XanTe focuses on finding the best yields and return across financial
+          markets by connecting Lenders and Borrowers, aggregating data points
+          in order to present consumers with offers in real time.
+        </p>
+        <div class="flex space-x-4 items-center mt-14">
+          <nuxt-link to="" class="central">Centralised</nuxt-link>
+          <nuxt-link to="" class="central">Decentralised</nuxt-link>
+        </div>
+      </div>
+      <div class="w1/2">
+        <img src="/images/network.png" class="w-full" alt="" />
+      </div>
+    </div>
+    <div class="comparing flex justify-between items-center">
+      <div class="lend w-1/2">
+        <h2>Compare borrowing and lending</h2>
+        <p class="mt-4 font-thin">
+          XanTe searches across centralised and Decentralised cryptocurrency and
+          traditional financial markets.
+        </p>
+      </div>
+      <div class="w-1/2">
+        <img src="/images/compare.png" class="w-full" alt="" />
+      </div>
+    </div>
+    <div class="data flex justify-between items-center">
+      <div class="w-1/2">
+        <img src="/images/phone.png" class="w-1/2" alt="" />
+      </div>
+      <div class="points w-1/2">
+        <h2>
+          XanTe compares and displays aggregated data points to enable users
+          make smart and informed decisions.
+        </h2>
+        <p class="mt-4 font-thin">
+          XanTe brings lenders and borrowers to a unified platform across
+          decentralised protocols and centralised providers.
+        </p>
+      </div>
+    </div>
+    <div class="smart flex justify-between items-center">
+      <div class="finance w-1/2">
+        <h2>Smart Financial Decisions</h2>
+        <p class="mt-4 font-thin">
+          XanTe empowers users to make smart decisions by providing useful
+          financial decisioning tools like Earnings Calculator, Reward finders,
+          Wallet aggregators
+        </p>
+      </div>
+      <div class="w-1/2">
+        <img src="/images/smart.png" class="w-full" alt="" />
+      </div>
+    </div>
+    <div class="users flex justify-between items-center">
+      <div class="w-1/2">
+        <img src="/images/users.png" class="w-4/5" alt="" />
+      </div>
+      <div class="use w-1/2">
+        <h2 class="font-thin">
+          <b>XanTe</b> empowers users to make smart decisions by providing
+          useful financial decisioning tools like Earnings Calculator, Reward
+          finders, Wallet aggregators
+        </h2>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.cover {
+  background: rgba(145, 20, 255, 0.08);
+}
+.crypto h1 {
+  font-family: 'Sharp-sans' !important;
+  line-height: 60px !important;
+}
+.crypto p {
+  width: 81%;
+  line-height: 24px !important;
+}
+.central {
+  background: #3f318c;
+  padding: 20px 70px;
+  border-radius: 4px;
+}
+.comparing {
+  background: #081b3d;
+  padding: 148px 181px;
+}
+.lend h2 {
+  font-family: 'Sharp-sans' !important;
+  width: 40%;
+  font-weight: 600;
+  line-height: 48px;
+}
+.lend p {
+  width: 55%;
+  line-height: 24px !important;
+}
+.data {
+  background: #0a132b;
+  padding: 23px 181px 116px 181px;
+}
+.points h2 {
+  font-family: 'Sharp-sans' !important;
+}
+.points p {
+  width: 50%;
+  line-height: 24px !important;
+}
+.smart {
+  background: #081b3d;
+  padding: 64px 181px 86px 181px;
+}
+.smart h2 {
+  font-family: 'Sharp-sans' !important;
+  width: 80%;
+}
+.smart p {
+  width: 70%;
+  line-height: 24px !important;
+}
+.users {
+  background: #0a132b;
+  padding: 22px 181px 129px 181px;
+}
+.use h2 {
+  /* font-family: 'Sharp-sans' !important; */
+  font-weight: 400;
+  font-size: 27px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,117 +1,18 @@
 <template>
-  <div class="m-0 p-0">
-    <!-- <div class="header-sect" style=""> -->
-
-    <!-- <div class="" style="">
-        <h1 class="" style="width: 513px">Smart & Intelligent Search across Cryptocurrency & Traditional Markets for the best return on your assets.</h1>
-        <p class="mt-6" style="line-height: 148.3%; font-weight: 400; width:584px;">XanTe focuses on finding the best yields and return across financial markets by connecting Lenders and Borrowers, aggregating data points in order to present consumers with offers in real time.</p>
-        <div class="mt-14 flex items-center">
-          <button class="btn prybtn py-4 px-16 mr-8">Centralized Market</button>
-          <button class="btn prybtn py-4 px-14">Decentralized Market</button>
-        </div>
-      </div> -->
-
-    <!-- <div class="AAG overflow-x-hidden"> -->
-    <!-- <img class="" src="../assets/images/AAAG 1.png" alt=""> -->
-    <!-- <img class="ellip" src="../assets/images/Ellipse 71.png" alt=""> -->
-    <!-- <img class="vec17" src="../assets/images/Vector 17.png" alt=""> -->
-    <!-- <img class="vec172" src="../assets/images/Vector 17.png" alt=""> -->
-    <!-- </div> -->
-    <!-- </div> -->
-
-    <!-- <div class="sect-2 relative">
-      <div class="w-1/5" style="">
-        <h2 class="mb-4">Compare borrowing and lending</h2>
-        <p class="mb-8">XanTe searches across centralised and Decentralised cryptocurrency and traditional financial markets.</p>
-        <button class="btn secbtn">Compare Now</button>
-      </div>
-
-      <div class="sect-2-img">
-         <div class="rec-box text-center">Lend APY<br> 25.5%</div>
-        <img src="../assets/images/shutterstock_481596547 1.png" alt="">
-        <div class="rec-box text-center">Borrow APY<br> 25.5%</div>
-      </div>
-    </div>    -->
+  <div class="">
+    <home-vue />
   </div>
 </template>
 
 <script>
-// import PryButton from '../components/Buttons/PryButton.vue'
+import homeVue from '~/components/home.vue'
 
 export default {
-  name: "index",
   components: {
-    // PryButton,
+    homeVue,
   },
-};
+}
 </script>
 
-<style scoped>
-.AAG {
-  height: 493px;
-  /* width: 820px;  */
-  position: absolute;
-  left: 565px;
-  top: 140px;
-  border-radius: 0px;
-}
-
-.ellip {
-  height: 645px;
-  width: 645px;
-  position: absolute;
-  left: 781px;
-  top: 145px;
-  border-radius: 0px;
-  background: rgba(145, 20, 255, 0.08);
-  filter: blur(100px);
-}
-
-.vec17 {
-  height: 563px;
-  width: 1024px;
-  position: absolute;
-  left: 34px;
-  top: 523px;
-  background: linear-gradient(
-    227.09deg,
-    #4e3daa -86.92%,
-    rgba(22, 32, 60, 0.05) 74.09%
-  );
-}
-
-.vec172 {
-  position: absolute;
-  width: 1024px;
-  height: 563px;
-  left: 2046px;
-  top: 493px;
-  background: linear-gradient(
-    227.09deg,
-    rgba(255, 255, 255, 0.15) -86.92%,
-    rgba(22, 32, 60, 0.05) 74.09%
-  );
-  transform: matrix(-1, 0, 0, 1, 0, 0);
-}
-
-.rec-box {
-  height: 80px;
-  width: 236px;
-  position: absolute;
-  left: 617px;
-  top: 936px;
-  border-radius: 10px;
-  box-shadow: 0px 4px 24px -1px #151a3733;
-  border: 0.5px solid;
-  border-image-source: linear-gradient(
-    105.43deg,
-    #9095a1 7.18%,
-    rgba(144, 149, 161, 0) 82.76%
-  );
-  background: linear-gradient(
-    108.73deg,
-    rgba(255, 255, 255, 0.4) 13.38%,
-    rgba(255, 255, 255, 0.1) 79.79%
-  );
-}
+<style >
 </style>
