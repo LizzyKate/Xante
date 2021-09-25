029702845fa7187b25bb94e4b72cb1c26a14fd3b
LizzyKate <onyenekwelizabeth@gmail.com>
2021-09-25 17:58:01 +0100

    fixes

--- pages/login.vue
+++ pages/login.vue
@@ -1,21 +1,25 @@
 <template>
-  <div>
-    <div>
-      <img
-        class="img1 xl:block hidden mx-auto pt-20"
-        src="/images/logo.png"
-        alt=""
-      />
+  <div class="pt-20 relative h-screen">
+    <div class="text-center xl:block hidden">
+      <button class="" @click="$router.push('/')">
+        <img class="img1 mx-auto" src="/images/logo.png" alt="" />
+      </button>
     </div>
-    <div>
-      <img
-        class="img1 xl:hidden block mx-auto pt-20"
-        src="/images/app_icon.png"
-        alt=""
-      />
+    <div class="text-center xl:hidden block">
+      <button @click="$router.push('/')">
+        <img class="img1 mx-auto" src="/images/app_icon.png" alt="" />
+      </button>
     </div>
-
     <login-vue />
+    <div class="absolute xl:block hidden bottom-0 right-0">
+      <img src="/images/first.png" alt="" />
+    </div>
+    <div class="absolute xl:block hidden bottom-0" style="right: 200px">
+      <img src="/images/half.png" alt="" />
+    </div>
+    <div class="absolute xl:block hidden right-0" style="bottom: 80px">
+      <img src="/images/fifth.png" alt="" />
+    </div>
   </div>
 </template>
 
--- pages/signup.vue
+++ pages/signup.vue
@@ -1,24 +1,25 @@
 <template>
-  <div class="pt-20">
-    <div class="text-center">
-      <button @click="$router.push('/')">
-        <img
-          class="img1 xl:block hidden mx-auto"
-          src="/images/logo.png"
-          alt=""
-        />
+  <div class="pt-20 relative h-screen">
+    <div class="text-center xl:block hidden">
+      <button class="" @click="$router.push('/')">
+        <img class="img1 mx-auto" src="/images/logo.png" alt="" />
       </button>
     </div>
-    <div class="text-center">
+    <div class="text-center xl:hidden block">
       <button @click="$router.push('/')">
-        <img
-          class="img1 xl:hidden block mx-auto"
-          src="/images/app_icon.png"
-          alt=""
-        />
+        <img class="img1 mx-auto" src="/images/app_icon.png" alt="" />
       </button>
     </div>
     <signup-vue />
+    <div class="absolute xl:block hidden bottom-0 right-0">
+      <img src="/images/first.png" alt="" />
+    </div>
+    <div class="absolute xl:block hidden bottom-0" style="right: 200px">
+      <img src="/images/half.png" alt="" />
+    </div>
+    <div class="absolute xl:block hidden right-0" style="bottom: 80px">
+      <img src="/images/fifth.png" alt="" />
+    </div>
   </div>
 </template>
 
